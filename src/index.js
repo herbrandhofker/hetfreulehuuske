1209d83f2131e814df3d2ad1de5e4ba8dba8bb36
Herbrand Hofker <herbrand.hofker@gmail.com>
2022-01-23 17:41:08 +0100

    a

--- src/index.js
+++ src/index.js
@@ -18,15 +18,34 @@ function createDagboek(parent) {
 
 function createAmelander(parent) {
     const amelanderDiv = parent.appendChild(document.createElement("div"))
-    amelanderDiv.id="amelander"
-    amelanderDiv.classList.add("epub-div")
-    const a = amelanderDiv.appendChild(document.createElement("a"))
-    a.innerText = "Naar top"
-    a.href="#home"
-
+    amelanderDiv.classList.add("de-amelander")
+    amelanderDiv.id="amelander";
+    const h1= amelanderDiv.appendChild(document.createElement("h3"));
+    h1.innerHTML=`<a href="./data/amelander/sept_2021.pdf" target="_blank">Klil hier voor artikel over Vermaningspad en Het Freule Huuske</a><br><i>wij vinden het
+    belangrijk dat dit historische pad in Nes behouden blijft. Amelander
+    Musea erkennen ook het historische belang van het Vermaningspad door
+    tijdens vakanties Free Tours te organiseren. (Zie <a href="https://www.vvvameland.nl/zien-en-doen/excursies/free-tour#:~:text=Stichting%20Amelander%20Musea%20organiseert%20free%20tours%20in%20de,de%20leukste%20introductie%20tot%20de%20cultuurhistorie%20van%20Ameland" target="_blank">Free Tour</a>) Deze vrijblijvende
+    rondleidingen leiden u langs de bijzondere geschiedenis van het
+    Vermaningspad en gaan over de bestuurlijke en religieuze geschiedenis
+    van Ameland en de walvisvaart.</i><br>(Boodschap van Jacob Roep en Afke Schols van 'Amelander Musea')`;
+     const deAmelander = amelanderDiv.appendChild(document.createElement("a"))
+    deAmelander.innerText="Maandblad 'de Amelander', (bijna) overal op Ameland te koop"
+    deAmelander.href="https://www.deamelander.nl/"
+    deAmelander.target="_blank"
+  
+    const yt = amelanderDiv.appendChild(document.createElement("a"))
+    yt.innerText="Freetour met bezoek Freule Huuske"
+    yt.href="https://www.youtube.com/watch?v=0Sc-1cLTR7A&ab_channel=AmelandVandaag"
+    yt.target="_blank"
+   
+     
+    const toTop = amelanderDiv.appendChild(document.createElement("a"))
+    toTop.innerText = "Naar top"
+    toTop.href="#home"
+/*
     const h1 = amelanderDiv.appendChild(document.createElement("h1"))
     h1.innerText = "De Amelander over het Vermaningspad (september 2021)"
-    epub(amelanderDiv, 'amelander', 2, "jpg");
+    epub(amelanderDiv, 'amelander', 2, "jpg");*/
 }
 
 fetch("./data/freulehuuske.json")
@@ -48,7 +67,7 @@ function doit(parent, data) {
     dagboekButton.innerText="Naar dagboek Willem Hofker";
     const amelanderButton= buttonContainer.appendChild(document.createElement("a"));
     amelanderButton.href="#amelander"
-    amelanderButton.innerText="Naar 'De Amelander' over het Vermaningspad";
+    amelanderButton.innerText="Naar Amelander Musea in 'De Amelander' over het Freule Huuske";
     for (let i = 0; i < data.sections.length; i++) {
         const section = data.sections[i];
         const sectionDiv = parent.appendChild(document.createElement("div"));
